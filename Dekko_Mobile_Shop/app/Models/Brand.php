5c4a5c5bdaab732c5393838087c3732a9b6d3071
sanjana <sanjanaabeysiri1234567@gmail.com>
2024-12-20 23:02:54 +0530

    Modify table : Add Brand table and, this table connect with SpareParts table Many To Many Connection

---
+++ Dekko_Mobile_Shop/app/Models/Brand.php
@@ -0,0 +1,23 @@
+<?php
+
+// app/Models/Brand.php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Brand extends Model
+{
+    use HasFactory;
+
+    protected $table = 'brands';
+    protected $primaryKey = 'model_category_id';
+    protected $fillable = ['brand_name', 'model_name', 'description'];
+
+    // Many-to-many relationship with spare_parts
+    public function spareParts()
+    {
+        return $this->belongsToMany(SparePart::class, 'brand_spare_part', 'brand_id', 'spare_part_id');
+    }
+}
